2812fd1c3a62c4ac5d43da3efb8ff5f48545f012
Sean Herrera <herrerasean3@gmail.com>
2018-07-31 16:56:16 -0400

    Completed Alarm Clock lab.

--- cssi-lab/javaScript/labs/Alarm_Clock/script.js
+++ cssi-lab/javaScript/labs/Alarm_Clock/script.js
@@ -13,3 +13,100 @@
 // limitations under the License.
 
 console.log("script is running...");
+
+function Basic_Alarm(alarmMessage){
+  return alarmMessage;
+}
+
+console.log(Basic_Alarm("My alarm!"));
+
+function myAlarm(time){
+  if (time === undefined) {
+    time = new Date()
+    time = `${time.getHours()}:${time.getMinutes()}`;
+  }
+  return `Hey, Sean, wake up! It's ${time}`
+}
+
+function Mom_Alarm(time){
+  if (time === undefined) {
+    time = new Date()
+    time = `${time.getHours()}:${time.getMinutes()}`;
+  }
+  return `Hey, Mom, wake up! It's ${time}`
+}
+
+function familyAlarm(time, name){
+  if (time === undefined) {
+    time = new Date();
+  }
+  if (name === undefined) {
+    name = "Todd Howard"
+  }
+  return `Hey, ${name}, wake up! It's ${time.getHours()}:${time.getMinutes()}`
+}
+
+function Important_Alarm(time, name, message){
+  if (time === undefined) {
+  time = new Date();
+}
+  if (name === undefined) {
+    name = "Todd Howard"
+  }
+  if (message === undefined){
+    return `Hey, ${name}, wake up! It's ${time.getHours()}:${time.getMinutes()}`;
+  } else {
+    return message;
+  }
+}
+
+function Snooze_Alarm(hour, minute, name, message, snooze){
+  if (hour === undefined) {
+  hour = new Date().getHours();
+  }
+  if (minute === undefined) {
+  minute = new Date().getMinutes();
+  }
+  let origHour = timeCorrect(hour);
+  let origMinute = timeCorrect(minute);
+  if (name === undefined) {
+    name = "Todd Howard"
+  }
+  if (snooze !== undefined){
+    if (minute + snooze > 59){
+      hour = hour + Math.floor((minute+snooze)/60);
+      if (hour >= 24){
+        hour = hour - 24;
+      }
+      hour = timeCorrect(hour);
+      minute = (minute+snooze)%60;
+      minute = timeCorrect(minute);
+    } else {
+      minute = minute + snooze;
+    }
+    hour = timeCorrect(hour);
+    let time = `${hour}:${minute}`;
+    return `The alarm for ${origHour}:${origMinute} has been changed to ${time}`;
+  }
+  else if (message === undefined){
+    return `Hey, ${name}, wake up! It's ${origHour}:${origMinute}`;
+  } else {
+    return message;
+  }
+}
+
+function timeCorrect(input){
+  if (input < 10) {
+    return "0" + input;
+  } else {
+    return input
+  }
+}
+
+//Extension
+
+function extensAlarm(times){
+  for (var i = 1; i < times; i++) {
+    console.log(`Wake up! It's ${time}!`)
+  }
+}
