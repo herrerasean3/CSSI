7527b53cce6fa89bbef3f781d34f12fa15fb7489
Sean Herrera <herrerasean3@gmail.com>
2018-08-14 16:34:26 -0400

    Completed hogwarts-query lab

--- cssi-lab/appEngine-DataStore/labs/hogwarts-query/console_commands.py
+++ cssi-lab/appEngine-DataStore/labs/hogwarts-query/console_commands.py
@@ -13,12 +13,22 @@ teachers = hogwarts_models.Teacher.query()
 t_filter = teachers.order(hogwarts_models.Teacher.years_experience)
 print t_filter.get().name
 
-enrolled = hogwarts_models.Enrollment.query(projection = ["name"],distinct=True)
+enrolled = hogwarts_models.Enrollment.query(projection = ["student"],distinct=True)
 course_max = 0
 en_query = hogwarts_models.Enrollment.query()
 
 for i in enrolled.fetch():
- if en_query.filter(hogwarts_models.Enrollment.student == i.key)
+ t = en_query.filter(hogwarts_models.Enrollment.student == i.student)
+ if t.count() > course_max:
+  student_key = i.student
+  course_max = t.count()
+
+print course_max
+
+m_enrolled = hogwarts_models.Student.query()
+m_filter = m_enrolled.filter(hogwarts_models.Student.key==student_key)
+
+print m_filter.get().first_name
 
 har_key = hogwarts_models.Student.query()
 har_filter = har_key.filter(hogwarts_models.Student.first_name=="Harry").get()
